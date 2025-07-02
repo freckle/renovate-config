c75180b667b42ee65160637bed7c17a316f0f863
patrick brisbin <pbrisbin@gmail.com>
2025-07-02 15:22:42 -0400

    renovate-audit improvements

--- renovate-audit/lib/renovate_audit/github/repository.rb
+++ renovate-audit/lib/renovate_audit/github/repository.rb
@@ -1,11 +1,12 @@
 module RenovateAudit
   module GitHub
     class Repository
-      attr_reader :full_name, :archived, :forked
+      attr_reader :name, :full_name, :archived, :forked
 
       def self.for_organization(client, organization)
         client.repos(organization).map do |repo|
           new(
+            name: repo.name,
             full_name: repo.full_name,
             archived: repo.archived,
             forked: repo.forked,
@@ -13,7 +14,8 @@ module RenovateAudit
         end
       end
 
-      def initialize(full_name:, archived:, forked:)
+      def initialize(name:, full_name:, archived:, forked:)
+        @name = name
         @full_name = full_name
         @archived = archived
         @forked = forked
